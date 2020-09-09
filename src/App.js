de915db7617697a54be164376bccd58607f7edb5
Gabriel Andrian <gabriel_andrian@hotmail.com>
2020-09-09 11:34:23 -0300

    - Routers inserted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Routers from './routers';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Routers />
         <div />
       </header>
     </div>
