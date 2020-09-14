206bbaf1b14ea9def70cb4fd3d1428e1e08e2736
Chrystian <chrystianrz@hotmail.com>
2020-09-14 09:26:02 -0300

    fix: store not found

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,1 +1,6 @@
 //To be added
+import { combineReducers } from 'redux';
+
+import shelfReducer from './shelfReducer';
+
+export default combineReducers({ shelfReducer });
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,1 +1,9 @@
 //To be added
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from '../reducers';
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+export default store;
